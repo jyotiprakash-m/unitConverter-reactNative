6fae528796cdebe4b7451db07aa663c99834c3ee
Jyotiprakash Mohanta <jpmohanta32@gmail.com>
2020-12-23 11:13:29 +0530

    Add unit convertion

--- App.js
+++ App.js
@@ -10,19 +10,64 @@
  * npm i react-native-gesture-handler
  */
 
-import React, { useState } from 'react'
-import { View, Text, StyleSheet, Dimensions } from 'react-native'
+import React, { useState,useEffect } from 'react'
+import { View, Text, StyleSheet, Dimensions,TextInput } from 'react-native'
 import {TabView,TabBar} from 'react-native-tab-view'
 import convert  from 'convert-units'
-// import Constants from 'expo-constants'
-
+import {Picker} from '@react-native-picker/picker'
 const measure = convert().measures()
 // console.log(measure)
 
 const mainColor="#3642cc"
 // Measure view function
 
-const MeasureView = ({measure}) => <Text>{measure}</Text>
+const MeasureView = ({measure,value,setValue}) => {
+
+  const units = convert().possibilities(measure)
+
+  const [formUnit,setFormUnit] = useState(units[0])
+  const [toUnit,setToUnit] = useState(units[1])
+
+  const [valueConverted,setValeConverted] = useState(0)
+
+
+  useEffect(()=>{
+    setValeConverted(convert(+value).from(formUnit).to(toUnit).toFixed(2))
+
+  },[value,formUnit,toUnit])
+
+  return(
+    <View style={styles.scene}>
+        <View style={styles.row}>
+          <Picker style={styles.column} selectedValue = {formUnit} onValueChange={setFormUnit}>
+              {units.map((unit,index) => (
+                <Picker.Item label={unit} value={unit} key ={index} />
+              ) )}
+          </Picker>
+          <View style={styles.column}>
+                <TextInput 
+                  value={value} 
+                  onChangeText={setValue}
+                  keyboardType='numeric'
+                  style={styles.input}
+                
+                />
+          </View>
+        </View>
+        <View style={styles.row}>
+          <Picker style={styles.column} selectedValue = {toUnit} onValueChange={setToUnit}>
+              {units.map((unit,index) => (
+                <Picker.Item label={unit} value={unit} key ={index} />
+              ) )}
+          </Picker>
+          <View style={styles.column}>
+                <Text style = {[styles.input,{fontSize:35,fontWeight:'bold',height:100}]}>{valueConverted} {''}</Text>
+          </View>
+        </View>
+    </View>
+    
+  )
+}
 
 
 export default function App() {
@@ -35,10 +80,12 @@ export default function App() {
   })))
 
   // render scene funcyion
+  const [value,setValue] = useState('0')
+
 
   const renderScene = ({route}) =>{
       return(
-        <MeasureView measure={route.key} />
+        <MeasureView measure={route.key} value={value} setValue={setValue} />
       )
   }
 
@@ -85,5 +132,22 @@ const styles = StyleSheet.create({
     fontSize:20,
     textAlign:'center',
     textTransform:'uppercase'
+  },
+  row:{
+    flex:1,
+    flexDirection:'row',
+    alignItems:'center',
+  },
+  column:{
+    flex:1,
+    marginLeft:10,
+    marginRight:10,
+  },
+  input:{
+    height:60,
+    fontSize:25,
+    borderColor:mainColor,
+    borderBottomWidth:1,
+    alignItems:'center'
   }
 })
